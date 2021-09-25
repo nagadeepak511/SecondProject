471fa6fc16331379ac4d6aa41c37939ae52e18f2
Naga Deepak Kaza <83226719+nagadeepak511@users.noreply.github.com>
2021-09-25 13:44:01 +0530

    codes and data collected

---
+++ tesla/shop/script.js
@@ -0,0 +1,7 @@
+function showCategoryContianer(){
+    document.getElementsByClassName('categories')[0].style.transform = 'translateY(60px)'
+}
+
+function hideCategory(){
+    document.getElementsByClassName('categories')[0].style.transform = 'translateY(-200%)'
+}
